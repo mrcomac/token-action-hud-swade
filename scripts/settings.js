80fe802234d75ba31a1c0270ff0200f546a402c8
Marcos <marcos.rafael.machado@gmail.com>
2023-12-03 19:24:00 +0000

    fix default config

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,8 +1,10 @@
 import { MODULE } from './constants.js'
 
 export function register (updateFunc) {
+    console.log("REGISTER SETTING2")
     const targetModule = game.modules.get("betterrolls-swade2");
     if (targetModule && targetModule.active) {
+        console.log("REGISTER SETTING22")
         game.settings.register(MODULE.ID, 'br2RollsBehaviour', {
             name: "Select HUD's behavour when using Better Rolls module",
             hint: "By selecting one of the options from the dropdown menu, you can tailor the behavior of the HUD module to simulate ctrl and alt keys pressed.This customization enhances your experience with the Better Rolls module.",
@@ -16,7 +18,7 @@ export function register (updateFunc) {
                 "alt_click": "Alt click action",
                 "ctrl_click": "Crtl click action"
             },
-            default: "single",
+            default: "click",
             onChange: (value) => {
                 updateFunc(value)
             }
